2057fabc9657874fde59e87230c6f4ee17786bbf
Nour El-Din Shobier <nour.eldin.shobier@gmail.com>
2020-09-18 00:15:29 +0200

    Add IBM font

--- lib/styles/text.style.dart
+++ lib/styles/text.style.dart
@@ -1,9 +1,9 @@
 import 'package:stylex/stylex.dart';
 
 final textStyle = StyleX({
-  'font-family-primary-en': 'ProductSans-Regular',
-  'font-family-primary-light-en': 'ProductSans-Light',
-  'font-family-primary-medium-en': 'ProductSans-Medium',
-  'font-family-primary-semibold-en': 'ProductSans-SemiBold',
-  'font-family-primary-bold-en': 'ProductSans-Bold',
+  'font-family-primary': 'IBMPlexSans-Regular',
+  'font-family-primary-light': 'IBMPlexSans-Light',
+  'font-family-primary-medium': 'IBMPlexSans-Medium',
+  'font-family-primary-semibold': 'IBMPlexSans-SemiBold',
+  'font-family-primary-bold': 'IBMPlexSans-Bold',
 });
