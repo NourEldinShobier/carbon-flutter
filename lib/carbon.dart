5adf7946720c4d5b152c2ba24798fbe1a1451ee9
Nour El-Din Shobier <nour.eldin.shobier@gmail.com>
2020-09-18 03:48:41 +0200

    Finish CText props & styles attributes

--- lib/carbon.dart
+++ lib/carbon.dart
@@ -1,10 +1,10 @@
 library carbon;
 
 // TODO:
-// 1) Finish all text props.
 // 1.1) Add String extensions
 // 2) Finish Material themeData & default flutter font
-// 2) Finish all MaterialApp props
+// 3) Finish all MaterialApp props
+// 4) add textfield widget
 
 // you can make stylestore generic Stylestore<Carbon>
 // make defualt material font == carbon-primary font
--- lib/widgets/common/text.widget.dart
+++ lib/widgets/common/text.widget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CText extends StatelessWidget {
-  const CText({Key key, @required this.props}) : super(key: key);
+  const CText({Key key, @required this.props, this.style}) : super(key: key);
 
   final Map<String, dynamic> props;
+  final Map<String, dynamic> style;
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +13,22 @@ class CText extends StatelessWidget {
       textAlign: props['textAlign'] ?? null,
       maxLines: props['maxLines'] ?? null,
       overflow: props['overflow'] ?? null,
+      textDirection: props['textDirection'] ?? null,
+      locale: props['locale'] ?? null,
+      textScaleFactor: props['textScaleFactor'] ?? null,
+      semanticsLabel: props['semanticsLabel'] ?? null,
+      textWidthBasis: props['textWidthBasis'] ?? null,
+      textHeightBehavior: props['textHeightBehavior'] ?? null,
       style: TextStyle(
-        fontSize: props['fontSize'] ?? null,
-        fontFamily: props['fontFamily'] ?? null,
-        height: props['height'] ?? null,
-        color: props['color'] ?? null,
+        fontSize: style['fontSize'] ?? null,
+        height: style['height'] ?? null,
+        color: style['color'] ?? null,
+        backgroundColor: style['backgroundColor'] ?? null,
+        fontWeight: style['fontWeight'] ?? null,
+        fontStyle: style['fontStyle'] ?? null,
+        textBaseline: style['textBaseline'] ?? null,
+        locale: style['locale'] ?? null,
+        debugLabel: style['debugLabel'] ?? null,
       ),
     );
   }
