4d78e97e7bce97b4488aa284ed37d2bb71172fbd
Nour El-Din Shobier <nour.eldin.shobier@gmail.com>
2020-09-19 05:21:08 +0200

    G100 ThemeData v0.3.0

--- lib/styles/g100.theme.dart
+++ lib/styles/g100.theme.dart
@@ -3,7 +3,18 @@ import 'package:carbon/carbon.dart';
 import 'package:flutter/material.dart';
 import 'package:stylex/stylex.dart';
 
-final _materialTheme = ThemeData();
+final _materialTheme = ThemeData(
+  scaffoldBackgroundColor: carbonColors.get<Color>('carbon-gray-100'),
+  primaryColor: carbonColors.get<Color>('carbon-blue-60'),
+  accentColor: carbonColors.get<Color>('carbon-gray-10'),
+  disabledColor: carbonColors.get<Color>('carbon-gray-80'), // to define & test
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+      color: carbonColors.get<Color>('carbon-gray-10'),
+      fontFamily: carbonFonts.get<String>('carbon-font-family'),
+    ),
+  ),
+);
 
 final g100 = StyleX({
   'material-theme': _materialTheme,
