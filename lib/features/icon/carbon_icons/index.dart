aaeece59c0ab6360ad645356bfdeb5f4a3c82e7f
Nour El-Din Shobier <nour.eldin.shobier@gmail.com>
2021-07-09 13:27:46 +0200

    refactor: combine all carbon icons into one font

--- lib/features/icon/carbon_icons/index.dart
+++ lib/features/icon/carbon_icons/index.dart
@@ -1,6 +1,1 @@
-export 'icons_16.dart';
-export 'icons_20.dart';
-export 'icons_24.dart';
-export 'icons_32.dart';
-export 'icons_32_watson_health.dart';
-export 'icons_32_q.dart';
+export 'icons.dart';
