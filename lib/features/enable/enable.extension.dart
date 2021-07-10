efb7245080d2ddebd6bc12705b49e88d09fa428f
Nour Shobier <nour.eldin.shobier@gmail.com>
2021-07-10 13:53:58 +0200

    refactor(enable): refactor inheritedEnable extension to return bool always

--- lib/features/enable/enable.extension.dart
+++ lib/features/enable/enable.extension.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'enable.widget.dart';
 
 extension CEnableContextExtension on BuildContext {
-  bool? get inheritedEnable {
-    return CEnable.of(this);
+  bool get inheritedEnable {
+    final temp = CEnable.of(this);
+
+    return temp == null ? true : temp;
   }
 }
