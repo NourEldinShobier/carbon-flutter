0e274adf1d79059fc63f49b20cfead1edfdaa680
Nour Shobier <nour.eldin.shobier@gmail.com>
2021-07-12 09:13:10 +0200

    feat(shared-styles): add Thin and ExtraLight font, and add cTextTheme

--- lib/shared/styles/fonts.style.dart
+++ lib/shared/styles/fonts.style.dart
@@ -1,6 +1,8 @@
 abstract class CFonts {
-  static const primaryRegular = 'IBMPlexSans-Regular';
+  static const primaryThin = 'IBMPlexSans-Thin';
+  static const primaryExtraLight = 'IBMPlexSans-ExtraLight';
   static const primaryLight = 'IBMPlexSans-Light';
+  static const primaryRegular = 'IBMPlexSans-Regular';
   static const primaryMedium = 'IBMPlexSans-Medium';
   static const primarySemibold = 'IBMPlexSans-SemiBold';
   static const primaryBold = 'IBMPlexSans-Bold';
---
+++ lib/shared/styles/typography.style.dart
@@ -0,0 +1,20 @@
+import 'package:carbon/shared/index.dart';
+import 'package:flutter/material.dart';
+
+abstract class CTypography {
+  static const cTextTheme = TextTheme(
+    headline1: TextStyle(fontFamily: CFonts.primaryLight, package: 'carbon'),
+    headline2: TextStyle(fontFamily: CFonts.primaryLight, package: 'carbon'),
+    headline3: TextStyle(fontFamily: CFonts.primaryRegular, package: 'carbon'),
+    headline4: TextStyle(fontFamily: CFonts.primaryRegular, package: 'carbon'),
+    headline5: TextStyle(fontFamily: CFonts.primaryRegular, package: 'carbon'),
+    headline6: TextStyle(fontFamily: CFonts.primaryMedium, package: 'carbon'),
+    subtitle1: TextStyle(fontFamily: CFonts.primaryRegular, package: 'carbon'),
+    subtitle2: TextStyle(fontFamily: CFonts.primaryMedium, package: 'carbon'),
+    bodyText1: TextStyle(fontFamily: CFonts.primaryRegular, package: 'carbon'),
+    bodyText2: TextStyle(fontFamily: CFonts.primaryRegular, package: 'carbon'),
+    button: TextStyle(fontFamily: CFonts.primaryMedium, package: 'carbon'),
+    caption: TextStyle(fontFamily: CFonts.primaryRegular, package: 'carbon'),
+    overline: TextStyle(fontFamily: CFonts.primaryRegular, package: 'carbon'),
+  );
+}
