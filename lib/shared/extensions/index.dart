3e76bc793d2bce27bd1531f8b068b180b1f3fb28
Nour El-Din Shobier <nour.eldin.shobier@gmail.com>
2021-08-26 16:50:42 +0200

    feat(shared: extensions): add padding from list extension

--- lib/shared/extensions/index.dart
+++ lib/shared/extensions/index.dart
@@ -7,3 +7,19 @@ extension CThemeContextExtension on BuildContext {
     return this.fetch<CTheme>();
   }
 }
+
+extension CDoubleListExtension on List<double> {
+  EdgeInsetsGeometry get padding {
+    if (this.length == 1) {
+      return EdgeInsets.all(this[0]);
+    } else if (this.length == 2) {
+      return EdgeInsets.symmetric(vertical: this[0], horizontal: this[1]);
+    } else if (this.length == 4) {
+      return EdgeInsets.only(left: this[0], top: this[1], right: this[2], bottom: this[3]);
+    }
+
+    throw Exception('[CDoubleListExtension] : list length should be 1, 2 or 4');
+  }
+
+  get margin => padding;
+}
