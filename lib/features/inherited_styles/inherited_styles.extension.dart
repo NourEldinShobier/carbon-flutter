b3e3e567c1715c3f6c12183f6a5e652c7a40df9e
Nour Shobier <47837759+NourEldinShobier@users.noreply.github.com>
2021-11-21 18:38:28 +0200

    refactor(inherited_styles): :recycle: rename the extension method to styles

--- lib/features/inherited_styles/inherited_styles.extension.dart
+++ lib/features/inherited_styles/inherited_styles.extension.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'inherited_styles.widget.dart';
 
 extension CInheritedStylesContextExtension on BuildContext {
-  Map<String, dynamic> get inheritedStyles {
+  Map<String, dynamic> get styles {
     final inheritedStyles = CInheritedStyles.of(this);
 
     if (inheritedStyles == null) return {};
