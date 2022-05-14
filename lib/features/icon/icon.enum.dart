bfe23f4991adeb54e0208bb8bda43ff7c0d925dc
Nour Shobier <nour.eldin.shobier@gmail.com>
2022-05-14 14:26:33 +0200

    refactor(icon): remove enable parameter and remove svg_icon

--- lib/features/icon/icon.enum.dart
+++
@@ -1,1 +0,0 @@
-enum CSvgIconType { asset, network, file, memory, string }
--- lib/features/icon/index.dart
+++ lib/features/icon/index.dart
@@ -1,4 +1,1 @@
 export 'carbon_icons/index.dart';
-export 'models/index.dart';
-export 'svg_icon.widget.dart';
-export 'icon.widget.dart';
