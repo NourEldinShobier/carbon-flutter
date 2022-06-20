2f286dd7bd996ae97f5b46382ade68feb1ec006b
Nour Shobier <nour.eldin.shobier@gmail.com>
2022-06-20 20:39:53 +0200

    feature(interited_styles): enable inherting styles from parent

--- lib/features/inherited_styles/inherited_styles.extension.dart
+++ lib/features/inherited_styles/inherited_styles.extension.dart
@@ -3,11 +3,11 @@ import 'package:flutter/widgets.dart';
 import 'inherited_styles.widget.dart';
 
 extension CInheritedStylesContextExtension on BuildContext {
-  Map<String, dynamic> get styles {
+  StylesMap get styles {
     final inheritedStyles = CInheritedStyles.of(this);
 
     if (inheritedStyles == null) return {};
 
-    return inheritedStyles.styles;
+    return inheritedStyles;
   }
 }
--- lib/features/inherited_styles/inherited_styles.widget.dart
+++ lib/features/inherited_styles/inherited_styles.widget.dart
@@ -1,18 +1,61 @@
 import 'package:flutter/widgets.dart';
 
-class CInheritedStyles extends InheritedWidget {
+import 'inherited_styles.extension.dart';
+
+typedef StylesMap = Map<String, dynamic>;
+
+class CInheritedStyles extends StatelessWidget {
   const CInheritedStyles({
     Key? key,
     required this.styles,
-    required Widget child,
-  }) : super(key: key, child: child);
+    required this.child,
+    this.inheritFromParent = true,
+  }) : super(key: key);
 
-  final Map<String, dynamic> styles;
+  final Widget child;
 
-  static CInheritedStyles? of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<CInheritedStyles>();
+  final StylesMap styles;
+
+  /// Whether the current widget should inherit the styles or
+  /// only use the local [value].
+  final bool inheritFromParent;
+
+  static StylesMap? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<_CInheritedStylesWidget>()?.value;
+  }
+
+  StylesMap _styles(BuildContext context) {
+    if (inheritFromParent) {
+      final parentStyles = context.styles;
+      final StylesMap stylesClone = {};
+
+      stylesClone.addAll(styles);
+      stylesClone.addAll(parentStyles);
+
+      return stylesClone;
+    }
+
+    return styles;
   }
 
   @override
-  bool updateShouldNotify(CInheritedStyles oldWidget) => styles != oldWidget.styles;
+  Widget build(BuildContext context) {
+    return _CInheritedStylesWidget(
+      value: _styles(context),
+      child: child,
+    );
+  }
+}
+
+class _CInheritedStylesWidget extends InheritedWidget {
+  const _CInheritedStylesWidget({
+    Key? key,
+    required this.value,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final StylesMap value;
+
+  @override
+  bool updateShouldNotify(_CInheritedStylesWidget old) => value != old.value;
 }
