1fd346f0db6fa120db5cf846f18933f15f4451ef
HenriqueNas <henriquenas.dev@gmail.com>
2023-05-20 19:31:08 -0300

    docs(example): :memo: update CTag example documentation of usage

--- example/lib/examples/tag.example.dart
+++ example/lib/examples/tag.example.dart
@@ -10,17 +10,97 @@ class CTagExample extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         padding: EdgeInsets.all(44),
-        child: Wrap(
-          spacing: 8,
-          runSpacing: 8,
-          children: CTagColors.values
-              .map((color) => CTag(
-                    label: color.name,
-                    color: color,
-                  ))
-              .toList(),
+        // color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TagsExample(
+                title: 'default CTag widget',
+                children: CTagColors.values
+                    .map((color) => CTag(
+                          label: color.name,
+                          color: color,
+                        ))
+                    .toList(),
+              ),
+              TagsExample(
+                title: 'CTag with filter (CTag.filter)',
+                children: CTagColors.values
+                    .map((color) => CTag.filter(
+                          label: color.name,
+                          color: color,
+                        ))
+                    .toList(),
+              ),
+              TagsExample(
+                title: 'CTag with small size (CTag.size = CTagSize.sm)',
+                children: CTagColors.values
+                    .map((color) => CTag(
+                          label: color.name,
+                          color: color,
+                          size: CTagSize.sm,
+                        ))
+                    .toList(),
+              ),
+              TagsExample(
+                title: 'CTag with small size and filter',
+                children: CTagColors.values
+                    .map((color) => CTag.filter(
+                          label: color.name,
+                          color: color,
+                          size: CTagSize.sm,
+                        ))
+                    .toList(),
+              ),
+              TagsExample(
+                title: 'CTag with disabled state (CTag.disabled = true)',
+                children: [
+                  CTag(
+                    label: 'Disabled',
+                    disabled: true,
+                  ),
+                  CTag.filter(
+                    label: 'Disabled',
+                    disabled: true,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class TagsExample extends StatelessWidget {
+  const TagsExample({
+    super.key,
+    required this.title,
+    required this.children,
+  });
+
+  final String title;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(height: 40),
+        Text(title),
+        SizedBox(height: 12),
+        Wrap(
+          spacing: 8,
+          runSpacing: 8,
+          children: children,
+        ),
+      ],
     );
   }
 }
